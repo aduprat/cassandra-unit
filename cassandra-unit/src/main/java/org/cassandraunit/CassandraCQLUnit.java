95b27f5d82979647267ee47a98e9b01d854b6ecd
Antoine Duprat <antduprat@gmail.com>
2015-03-13 14:44:42 +0100

    Add retry policy in cluster configuration

--- cassandra-unit/src/main/java/org/cassandraunit/CassandraCQLUnit.java
+++ cassandra-unit/src/main/java/org/cassandraunit/CassandraCQLUnit.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.QueryOptions;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.policies.RetryPolicy;
 /**
  * @author Marcin Szymaniuk
  * @author Jeremy Sevellec
@@ -16,6 +19,7 @@ public class CassandraCQLUnit extends BaseCassandraUnit {
     private static final Logger log = LoggerFactory.getLogger(CassandraCQLUnit.class);
     private String hostIp = "127.0.0.1";
     private int port = 9142;
+    private RetryPolicy retryPolicy;
     public Session session;
     public Cluster cluster;
 
@@ -44,12 +48,31 @@ public class CassandraCQLUnit extends BaseCassandraUnit {
         this.port = port;
     }
 
+    public CassandraCQLUnit(CQLDataSet dataSet, String configurationFileName, String hostIp, int port, long startUpTimeout, RetryPolicy retryPolicy) {
+        this(dataSet, configurationFileName, hostIp, port, startUpTimeout);
+		this.retryPolicy = retryPolicy;
+    }
+
     protected void load() {
-        cluster = new Cluster.Builder().addContactPoints(hostIp).withPort(port).build();
+        cluster = cluster();
         session = cluster.connect();
         CQLDataLoader dataLoader = new CQLDataLoader(session);
         dataLoader.load(dataSet);
-        session = dataLoader.getSession();
+//        session = dataLoader.getSession();
     }
+
+	private Cluster cluster() {
+		Cluster.Builder builder = new Cluster.Builder()
+			.addContactPoints(hostIp)
+			.withPort(port)
+			.withQueryOptions(new QueryOptions()
+			.setConsistencyLevel(ConsistencyLevel.QUORUM));
+		
+		if (retryPolicy != null) {
+			builder.withRetryPolicy(retryPolicy);
+		}
+		
+		return builder.build();
+	}
 
 }
